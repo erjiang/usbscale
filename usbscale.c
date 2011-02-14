8c039b082c11d3176974df0a1fd7d80a22dae033
Eric Jiang <erjiang@indiana.edu>
2011-02-14 00:00:24 -0500

    Added gplv3 note to usbscale.c

--- usbscale.c
+++ usbscale.c
@@ -8,6 +8,23 @@
 // There are no options. **usbscale** will try to read data from the first
 // scale it finds when enumerating your USB devices.
 //
+/*
+usbscale
+Copyright (C) 2011 Eric Jiang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdint.h>
@@ -30,11 +47,6 @@
 //
 #include "scales.h"
 
-/*
- * These constants are from libhid/include/constants.h
- * and usage based on libhid/src/hid_exchange.c
- * also vvvoutput.txt
- */
 #define WEIGH_REPORT_SIZE 0x06
 
 //
