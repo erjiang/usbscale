a9190a57031f3c8ae975ea79291c50470f6f9802
Eric Jiang <erjiang@indiana.edu>
2011-02-13 20:39:00 -0500

    Fixed array in scales.h

--- scales.h
+++ scales.h
@@ -4,10 +4,11 @@
  *
  * For example, the USB product 1446:6173 becomes {0x1446, 0x6173}
  */
+#include <stdint.h>
 
 // array length
 int scalesc = 1;
 
-uint16_t scales[][] = {
-    {0x1446, 0x6173}    // Stamps.com USB scale
-}
+uint16_t scales[1][2] = {\
+    {0x1446, 0x6a73}    // Stamps.com USB scale
+};
