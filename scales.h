0268fa50b012236013c5741783d1b0a478317d13
Eric Jiang <erjiang@indiana.edu>
2011-02-14 20:20:19 -0500

    Updated documentation in scales.h

--- scales.h
+++ scales.h
@@ -1,14 +1,24 @@
-/*
- * This file is a list of all currently-recognized scales,
- * by their vendor and product IDs.
- *
- * For example, the USB product 1446:6173 becomes {0x1446, 0x6173}
- */
+//
+// scales.h
+// ========
+//
+// This file is a list of all currently-recognized scales' vendor and product
+// IDs.
+//
+// For example, the USB product 1446:6173 becomes {0x1446, 0x6173}
+//
 #include <stdint.h>
 
-// array length
+//
+// **scalesc** should be kept updated with the length of the list.
+//
 int scalesc = 1;
 
+//
+// Scales
+// ------
+//
 uint16_t scales[1][2] = {\
-    {0x1446, 0x6a73}    // Stamps.com USB scale
+    // Stamps.com 10-lb USB scale
+    {0x1446, 0x6a73}
 };
