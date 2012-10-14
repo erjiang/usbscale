5acc91ec4f1d7c14f72c40ff4d739c77a17c27f3
Eric Jiang <erjiang@indiana.edu>
2012-10-14 00:19:57 -0700

    Added "Stamps.com Stainless Steel 5 lb. ..."

--- scales.h
+++ scales.h
@@ -12,15 +12,17 @@
 //
 // **scalesc** should be kept updated with the length of the list.
 //
-int scalesc = 2;
+int scalesc = 3;
 
 //
 // Scales
 // ------
 //
-uint16_t scales[2][2] = {\
-    // Stamps.com 10-lb USB scale
+uint16_t scales[3][2] = {\
+    // Stamps.com Model 510 5LB Scale
     {0x1446, 0x6a73},
     // USPS (Elane) PS311 "XM Elane Elane UParcel 30lb"
-    {0x7b7c, 0x0100}
+    {0x7b7c, 0x0100},
+    // Stamps.com Stainless Steel 5 lb. Digital Scale
+    {0x2474, 0x0550}
 };
